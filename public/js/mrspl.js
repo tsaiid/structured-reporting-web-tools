631fa6b6887ee9ec7a2891fccec1bb49398995c6
I-Ta Tsai <ittsai@gmail.com>
2014-04-28 18:47:21 +0800

    Control radio buttons
    can toggle, only choose one.

--- public/js/mrspl.js
+++ public/js/mrspl.js
@@ -9,4 +9,16 @@ $(function(){
     a1 = $('#disc-rating-l12').rating('get rating');
     // alert(a1);
   });
+
+  $('.ui.button').on('click', function(){
+    if ($(this).hasClass('active')) {
+      $(this).removeClass('active');
+    } else {
+      $(this)
+        .addClass('active')
+        .siblings()
+        .removeClass('active')
+      ;
+    }
+  });
 });
