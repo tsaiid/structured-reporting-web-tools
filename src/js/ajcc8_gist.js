6547bbf68d6d3ece70ce97eb28e4a35039e40b72
I-Ta Tsai @ 8700k <ittsai@gmail.com>
2019-11-12 23:03:11 +0800

    fix

--- src/js/ajcc8_gist.js
+++ src/js/ajcc8_gist.js
@@ -2,7 +2,7 @@ import './common.js';
 import '../css/dashboard.css';
 import '../css/ajcc8_common.css';
 if (process.env.NODE_ENV !== 'production') {
-    require('raw-loader!../html/ajcc8/gastric.html');
+    require('raw-loader!../html/ajcc8/gist.html');
 }
 
 import {join_checkbox_values, ajcc_template} from './ajcc8_common.js';
