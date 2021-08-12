af2fbadd3a6ce46c8055fbf5790c989abad13bf6
I-Ta Tsai @ 8700k <ittsai@gmail.com>
2021-08-13 00:47:59 +0800

    change output

--- src/js/ajcc8_hcc.js
+++ src/js/ajcc8_hcc.js
@@ -141,7 +141,7 @@ function generate_report(){
     report += "\n\n";
 
     // Other Findings
-    report += "6. Other findings:\n\n\n";
+    report += "6. Other findings\n\n\n";
 
     // AJCC staging reference text
     let t = t_stage.sort()[t_stage.length-1];
