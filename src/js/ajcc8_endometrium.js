6ea2f0a998faab49578cf145c5d33ec519ad4645
I-Ta Tsai @ 8700k <ittsai@gmail.com>
2021-08-17 00:10:22 +0800

    update em_ca

--- src/js/ajcc8_endometrium.js
+++ src/js/ajcc8_endometrium.js
@@ -5,9 +5,9 @@ if (process.env.NODE_ENV !== 'production') {
     require('raw-loader!../html/ajcc8/endometrium.html');
 }
 
-import {join_checkbox_values, ajcc_template} from './ajcc8_common.js';
+import {join_checkbox_values, ajcc_template, ajcc_template_with_parent} from './ajcc8_common.js';
 
-const AJCC8_ENDOMETRIUM_T = {
+const AJCC8_T = {
     'x': 'Primary tumor cannot be assessed',
     '0': 'No evidence of primary tumor',
     '1': 'Tumor confined to the corpus uteri, including endocervical glandular involvement',
@@ -19,7 +19,7 @@ const AJCC8_ENDOMETRIUM_T = {
     '3b': 'Vaginal involvement (direct extension or metastasis) or parametrial involvement',
     '4': 'Tumor invading the bladder mucosa and/or bowel mucosa (bullous edema is not sufficient to classify a tumor as T4)',
 };
-const AJCC8_ENDOMETRIUM_N = {
+const AJCC8_N = {
     'x': 'Regional lymph node cannot be assessed',
     '0': 'No regional lymph node metastasis',
     '0(i+)': 'Isolated tumor cells in regional lymph node(s) no greater than 0.2 mm',
@@ -30,145 +30,167 @@ const AJCC8_ENDOMETRIUM_N = {
     '2mi': 'Regional lymph node metastasis (greater than 0.2 mm but not greater than 2.0 mm in diameter) to para-aortic lymph nodes, with or without positive pelvic lymph nodes',
     '2a': 'Regional lymph node metastasis (greater than 2.0 mm in diameter) to para-aortic lymph nodes, with or without positive pelvic lymph nodes',
 };
-const AJCC8_ENDOMETRIUM_M = {
+const AJCC8_M = {
     '0': 'No distant metastasis (in this study)',
     '1': 'Distant metastasis (includes metastasis to inguinal lymph nodes intraperitoneal disease, lung, liver, or bone). (It excludes metastasis to pelvic or para-aortic lymph nodes, vagina, uterine serosa, or adnexa).',
 };
 
 function generate_report(){
-    var t_stage = ["0"];
+    var t_stage = [];
     var n_stage = ["0"];
     var m_stage = ["0"];
-    var report = "1. ";
-    //var report = "1. CT protocol\n";
+    var report = `1. Imaging modality
+  - Imaging by `;
 
     // Protocol
-    if ($('input[name="protocol_radios"]:checked').val() == 'mr') {
-        report += `MR protocol
-- Distended rectum with jelly
-- Abdomen and pelvis:
-  * T2: coronal  * DWI (b=400): coronal
-- Uterus:
-  * TSE T2: axial, coronal, sagittal  * DWI (b=1000), ADC: axial
-  * T1+FS: axial, sagittal  * T1+C+FS: axial, sagittal`;
+    if ($('input[name="protocol_radios"]:checked').val() == 'ct') {
+        report += `[+] CT scan  [ ] MRI`;
     } else {
-        report += `CT protocol
-Intravenous contrast injection
-Range: whole abdomen, slice thickness <= 5mm`;
+        report += `[ ] CT scan  [+] MRI`;
     }
     report += "\n\n";
 
     // Tumor size
-    report += "2. Tumor size\n";
-    if ($('#cb_ts_nm').is(':checked')) {
-        report += "--- Non-measurable";
+    let t_length = parseFloat($('#txt_ts_len').val());
+    report += "2. Tumor size";
+    if ($('#cb_ts_nm').is(':checked') || !t_length) {
+        report += `
+    [+] Non-measurable
+    [ ] Measurable: ___ cm (greatest dimension)`;
     } else {
-        let t_dia = parseFloat($('#txt_ts_dia').val());
-        report += "--- Measurable: Greatest diameter " + t_dia + " cm";
+        report += `
+    [ ] Non-measurable
+    [+] Measurable: ${t_length} cm (greatest dimension)`;
     }
     report += "\n\n";
 
     // Tumor invasion
-    report += "3. Tumor invasion\n";
-    if ($('#cb_ti_na').is(':checked')) {
-        report += "--- Not assessable\n";
-    } else {
-        if ($("input[name='rb_ti_uc']:checked").length) {
-            report += `Uterine corpus: ${ $("input[name='rb_ti_uc']:checked").next().text() }\n`;
-            t_stage.push($("input[name='rb_ti_uc']:checked").val());
-            //console.log(t_stage);
-        }
-        if ($('.cb_ti:checked').length) {
-            report += "--- Yes:\n";
-            report += "* " + join_checkbox_values($('.cb_ti:checked'), "\n* ");
-            report += "\n";
+    let has_ti = $('.cb_ti:checked').length > 0 || $("input[name='rb_ti_uc']:checked").length > 0;
+    let has_ti_na = $('#cb_ti_na').is(':checked');
+    let ti_na_check = has_ti_na && !has_ti ? "+" : " ";
+    let ti_no_check = !has_ti_na && !has_ti ? "+" : " ";
+    let ti_yes_check = has_ti ? "+" : " ";
+    let ti_uc_check = $("input[name='rb_ti_uc']:checked").length > 0 ? "+" : " ";
+    let ti_lhm_check = $('#rb_ti_lhm').is(':checked') ? "+" : " ";
+    let ti_mhm_check = $('#rb_ti_mhm').is(':checked') ? "+" : " ";
+    let ti_cx_check = $('#cb_ti_cx').is(':checked') ? "+" : " ";
+    let ti_sa_check = $('#cb_ti_sa').is(':checked') ? "+" : " ";
+    let ti_pm_check = $('#cb_ti_pm').is(':checked') ? "+" : " ";
+    let ti_va_check = $('#cb_ti_va').is(':checked') ? "+" : " ";
+    let ti_bd_check = $('#cb_ti_bd').is(':checked') ? "+" : " ";
+    let ti_bw_check = $('#cb_ti_bw').is(':checked') ? "+" : " ";
+    let ti_others_check = $('#cb_ti_others').is(':checked') ? "+" : " ";
+    let txt_ti_others = $('#txt_ti_others').val() ? $('#txt_ti_others').val() : "___";
 
-            if ($('.cb_ti_t2:checked').length) {
-                t_stage.push("2");
-            }
-            if ($('.cb_ti_t3a:checked').length) {
-                t_stage.push("3a");
-            }
-            if ($('.cb_ti_t3b:checked').length) {
-                t_stage.push("3b");
-            }
-            if ($('.cb_ti_t4:checked').length) {
+    report += `3. Tumor invasion
+  [${ti_na_check}] Not assessable
+  [${ti_no_check}] No or Equivocal
+  [${ti_yes_check}] Yes, if yes:
+    [${ti_uc_check}] Tumor in uterine corpus
+      [${ti_lhm_check}] Tumor limited to endometrium or invades less than one half of the myometrium
+      [${ti_mhm_check}] Tumor invades more than one half of the myometrium
+    [${ti_cx_check}] Tumor invades stromal connective tissue of the cervix but does not extend beyond uterus
+    [${ti_sa_check}] Tumor invades serosa &/or adnexa
+    [${ti_pm_check}] Parametrial involvement
+    [${ti_va_check}] Vaginal involvement
+    [${ti_bd_check}] Tumor invades bladder (mucosa)
+    [${ti_bw_check}] Tumor invades bowel (mucosa)
+    [${ti_others_check}] Others: ${txt_ti_others}`;
+    report += "\n\n";
+
+    if (has_ti_na && !has_ti) {
+        t_stage.push("x");
+    } else if ($('.cb_ti_t4:checked').length) {
                 t_stage.push("4");
-            }
-            //console.log(t_stage);
-        }
-        if ($('.cb_ti:not(:checked)').length) {
-            report += "--- No or Equivocal:\n";
-            report += "* " + join_checkbox_values($('.cb_ti:not(:checked)')) + "\n";
-        }
+    } else if ($('.cb_ti_t3b:checked').length) {
+        t_stage.push("3b");
+    } else if ($('.cb_ti_t3a:checked').length) {
+        t_stage.push("3a");
+    } else if ($('.cb_ti_t2:checked').length) {
+        t_stage.push("2");
+    } else if ($('#rb_ti_mhm').is(':checked')) {
+        t_stage.push("1b");
+    } else if ($('#rb_ti_lhm').is(':checked')) {
+        t_stage.push("1a");
+    } else {
+        t_stage.push("0");
     }
-    report += "\n";
 
     // Regional nodal metastasis
+    let has_rln = $('.cb_rn:checked').length > 0;
     report += "4. Regional nodal metastasis\n";
-    if ($('.cb_rn:checked').length) {
-        report += "--- Yes:\n";
-        report += "* " + join_checkbox_values($('.cb_rn:checked'), "\n* ");
-        report += "\n";
-        if ($('.cb_rn_n2:checked').length) {
-            n_stage.push("2");
+    report += "  [" + (has_rln ? " " : "+") + "] No or Equivocal\n";
+    report += "  [" + (has_rln ? "+" : " ") + "] Yes, if yes:\n";
+    $('.lb_rn').each(function(){
+        let cb_rn = $(this).attr('for');
+        if ($(this).hasClass('has_parts')) {
+            let check_or_not = $('.' + cb_rn + ':checked').length > 0 ? "+" : " ";
+            report += `    [${check_or_not}] ` + $(this).text() + ": ";
+            let parts = $('.' + cb_rn);
+            parts.each(function(i, e){
+                let check_or_not = $(this).is(':checked') ? "+" : " ";
+                report += `[${check_or_not}] ` + $(this).val();
+                if (i !== parts.length - 1) {
+                    report += "  ";
+                }
+            });
+            report += "\n";
         } else {
-            n_stage.push("1");
+            let check_or_not = $('#' + cb_rn).is(':checked') ? "+" : " ";
+            report += `    [${check_or_not}] ` + $(this).text() + "\n";
         }
-    } /* else {
-        report += "* No regional lymph node metastasis.\n";
-    } */
-    if ($('.cb_rn:not(:checked)').length) {
-        report += "--- No or Equivocal:\n";
-        if ($('.cb_rn:not(:checked)').length) {
-            report += "* " + join_checkbox_values($('.cb_rn:not(:checked)')) + "\n";
-        }
-    }
+    });
     report += "\n";
+
+    if ($('.cb_rn_n2:checked').length) {
+        n_stage.push("2");
+    } else if ($('.cb_rn:checked').length) {
+        n_stage.push("1");
+    }
 
     // Distant metastasis
-    report += "5. Distant metastasis (In this study)\n";
-    if ($('.cb_dm:checked').length) {
-        report += "--- Yes:\n";
-        if ($('.cb_dm_nrl:checked').length) {
-            report += "* non-regional lymph node: " + join_checkbox_values($('.cb_dm_nrl:checked'));
-            if ($('#cb_dm_nrl_others').is(':checked')) {
-                report += $('#txt_dm_nrl_others').val();
-            }
-            report += "\n";
-        }
-        if ($('.cb_dm:not(.cb_dm_nrl):checked').length) {
-            report += "* " + join_checkbox_values($('.cb_dm:not(.cb_dm_nrl):checked'), "\n* ");
-            if ($('#cb_dm_others:checked').length) {
-                report += $('#txt_dm_others').val();
-            }
-            report += "\n";
-        }
+    let has_dm = $('.cb_dm:checked').length > 0;
+    let dm_no_check = !has_dm ? "+" : " ";
+    let dm_yes_check = has_dm ? "+" : " ";
+    let dm_nrl_check = $('.cb_dm_nrl:checked').length > 0 ? "+" : " ";
+    let dm_nrl_i_check = $('.cb_dm_nrl:checked').length > 0 ? "+" : " ";
+    let dm_nrl_i_r_check = $('#cb_dm_nrl_i_r').is(':checked') ? "+" : " ";
+    let dm_nrl_i_l_check = $('#cb_dm_nrl_i_l').is(':checked') ? "+" : " ";
+    let dm_nrl_others_check = $('#cb_dm_nrl_others').is(':checked') ? "+" : " ";
+    let txt_dm_nrl_others = $('#txt_dm_nrl_others').val() ? $('#txt_dm_nrl_others').val() : "___";
+    let dm_pc_check = $('#cb_dm_pc').is(':checked') ? "+" : " ";
+    let dm_lu_check = $('#cb_dm_lu').is(':checked') ? "+" : " ";
+    let dm_li_check = $('#cb_dm_li').is(':checked') ? "+" : " ";
+    let dm_ad_check = $('#cb_dm_ad').is(':checked') ? "+" : " ";
+    let dm_bo_check = $('#cb_dm_bo').is(':checked') ? "+" : " ";
+    let dm_others_check = $('#cb_dm_others').is(':checked') ? "+" : " ";
+    let txt_dm_others = $('#txt_dm_others').val() ? $('#txt_dm_others').val() : "___";
+    report += `5. Distant metastasis (In this study)
+  [${dm_no_check}] No or Equivocal
+  [${dm_yes_check}] Yes, if yes:
+    [${dm_nrl_check}] Non-regional lymph nodes
+      [${dm_nrl_i_check}] Inguinal nodes: [${dm_nrl_i_r_check}] right  [${dm_nrl_i_l_check}] left
+      [${dm_nrl_others_check}] Others: ${txt_dm_nrl_others}
+    [${dm_pc_check}] Peritoneal cavity
+    [${dm_lu_check}] Lung
+    [${dm_li_check}] Liver
+    [${dm_ad_check}] Adrenal
+    [${dm_bo_check}] Bone
+    [${dm_others_check}] Others: ${txt_dm_others}`;
+    report += "\n\n";
+
+    if (has_dm) {
         m_stage.push("1");
-        //console.log(m_stage);
-    } /* else {
-        report += "* No distant metastasis in the scanned range.\n";
-    } */
-    if ($('.cb_dm:not(:checked)').length) {
-        report += "--- No or Equivocal:\n";
-        if (!$('.cb_dm_nrl:checked').length) {
-            report += "* non-regional lymph node\n";
-        }
-        report += "* " + join_checkbox_values($('.cb_dm:not(.cb_dm_nrl):not(:checked)')) + "\n";
     }
-    report += "\n";
 
     // Other Findings
-    report += "6. Other findings:\n\n\n";
+    report += "6. Other findings\n\n\n";
 
     // AJCC staging reference text
     let t = t_stage.sort()[t_stage.length-1];
     let n = n_stage.sort()[n_stage.length-1];
     let m = m_stage.sort()[m_stage.length-1];
-    let t_str = AJCC8_ENDOMETRIUM_T[t];
-    let n_str = AJCC8_ENDOMETRIUM_N[n];
-    let m_str = AJCC8_ENDOMETRIUM_M[m];
-    report += ajcc_template("Endometrial Carcinoma", t, t_str, n, n_str, m, m_str);
+    report += ajcc_template_with_parent("Cervical Carcinoma", t, AJCC8_T, n, AJCC8_N, m, AJCC8_M);
 
     $('#reportModalLongTitle').html("Endometrial Cancer Staging Form");
     $('#reportModalBody pre code').html(report);
