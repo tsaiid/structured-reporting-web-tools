f259e3983c4d9d7b434b391d7c5004b2ceb5a669
I-Ta Tsai @ 8700k <ittsai@gmail.com>
2021-09-13 22:07:17 +0800

    update gastric: T3 vs T4a

--- src/js/gastric.js
+++ src/js/gastric.js
@@ -50,36 +50,35 @@ function generate_report(){
     report += "\n\n";
 
     // Tumor location / size
+    let tl_c_check = $('#cb_tl_c').is(':checked') ? "+" : " ";
+    let tl_f_check = $('#cb_tl_f').is(':checked') ? "+" : " ";
+    let tl_lc_check = $('#cb_tl_lc').is(':checked') ? "+" : " ";
+    let tl_gc_check = $('#cb_tl_gc').is(':checked') ? "+" : " ";
+    let tl_a_check = $('#cb_tl_a').is(':checked') ? "+" : " ";
+    let tl_p_check = $('#cb_tl_p').is(':checked') ? "+" : " ";
+    let tl_others_check = $('#cb_tl_others').is(':checked') ? "+" : " ";
+    let txt_tl_others = $('#txt_tl_others').val() ? $('#txt_tl_others').val() : "___";
+    let has_ts_nm = $('#cb_ts_nm').is(':checked');
+    let t_length = parseFloat($('#txt_ts_len').val());
+    let ts_nm_check = has_ts_nm ? "+" : " ";
+    let ts_m_check = !has_ts_nm ? "+" : " ";
+    let txt_ts_len = t_length ? t_length : "___";
     report += `2. Tumor location / size
   - Location:
-`;
-    $('.cb_tl:not(#cb_tl_others)').each(function(i){
-        report += "    [" + ($(this).is(':checked') ? "+" : " ") + "] " + $(this).val() + "\n";
-    });
-    if ($('#cb_tl_others').is(':checked')) {
-        report += "    [+] Others: " + $('#txt_tl_others').val() + "\n";
-    } else {
-        report += "    [ ] Others: ___\n";
-    }
+    [${tl_c_check}] Cardia               [${tl_f_check}] Fundus       [${tl_lc_check}] Lesser curvature
+    [${tl_gc_check}] Greater curvature    [${tl_a_check}] Antrum       [${tl_p_check}] Pylorus
+    [${tl_others_check}] Others: ${txt_tl_others}
+  - Size:
+    [${ts_nm_check}] Non-measurable
+    [${ts_m_check}] Measurable: ${txt_ts_len} cm (greatest diameter)
 
-    report += "  - Size:";
-    if ($('#cb_ts_nm').is(':checked') || !$('#txt_ts_len').val()) {
-        report += `
-    [+] Non-measurable
-    [ ] Measurable: ___ cm (largest diameter)`;
-    } else {
-        let t_length = parseFloat($('#txt_ts_len').val());
-        report += `
-    [ ] Non-measurable
-    [+] Measurable: ${t_length} cm (greatest diameter)`;
-    }
-    report += "\n\n";
+`;
 
     // Tumor invasion depth
     report += "3. Tumor invasion depth\n";
     $('input[name="radios_tid"]').each(function(){
         var item_str = ($(this).is(':checked') ? '[+] ' : '[-] ');
-        report += "  " + item_str + $(this).next().text();
+        report += "    " + item_str + $(this).next().text();
         if ($(this).val() == "4b") {
             report += ', location: ' + $('#txt_tid_loc').val();
         }
@@ -90,20 +89,36 @@ function generate_report(){
 
     // Regional nodal metastasis
     let has_rln = $('.cb_rn:checked').length && $('#txt_rln_num').val() > 0;
+    let rn_no_check = !has_rln ? "+" : " ";
+    let rn_yes_check = has_rln ? "+" : " ";
     let rln_num = (has_rln ? parseInt($('#txt_rln_num').val()) : "___");
-    report += "4. Regional nodal metastasis\n";
-    report += "  [" + (has_rln ? " " : "+") + "] No or Equivocal\n";
-    report += "  [" + (has_rln ? "+" : " ") + "] Yes, if yes, number of suspicious lymph node: " + rln_num + ", and locations (specified as below):\n";
-    $('.cb_rn:not("#cb_rn_others")').each(function(){
-        report += "    [" + ($(this).is(':checked') ? "+" : " ") + "] " + $(this).val() + "\n";
-    });
-    if ($('#cb_rn_others').is(':checked')) {
-        report += "    [+] Others: " + $('#txt_rn_others').val();
-    } else {
-        report += "    [ ] Others: ___";
-    }
-    report += "\n";
+    let rn_rpc_check = $('#cb_rn_rpc').is(':checked') ? "+" : " ";
+    let rn_lpc_check = $('#cb_rn_lpc').is(':checked') ? "+" : " ";
+    let rn_lc_check = $('#cb_rn_lc').is(':checked') ? "+" : " ";
+    let rn_gc_check = $('#cb_rn_gc').is(':checked') ? "+" : " ";
+    let rn_sp_check = $('#cb_rn_sp').is(':checked') ? "+" : " ";
+    let rn_ip_check = $('#cb_rn_ip').is(':checked') ? "+" : " ";
+    let rn_lga_check = $('#cb_rn_lga').is(':checked') ? "+" : " ";
+    let rn_cha_check = $('#cb_rn_cha').is(':checked') ? "+" : " ";
+    let rn_ca_check = $('#cb_rn_ca').is(':checked') ? "+" : " ";
+    let rn_sh_check = $('#cb_rn_sh').is(':checked') ? "+" : " ";
+    let rn_sa_check = $('#cb_rn_sa').is(':checked') ? "+" : " ";
+    let rn_hp_check = $('#cb_rn_hp').is(':checked') ? "+" : " ";
+    let rn_mca_check = $('#cb_rn_mca').is(':checked') ? "+" : " ";
+    let rn_others_check = $('#cb_rn_others').is(':checked') ? "+" : " ";
+    let txt_rn_others = $('#txt_rn_others').val() ? $('#txt_rn_others').val() : "___";
+    report += `4. Regional nodal metastasis
+    [${rn_no_check}] No or Equivocal
+    [${rn_yes_check}] Yes, if yes, number of suspicious lymph node: ${rln_num}, and locations (specified as below):
+        [${rn_rpc_check}] Right paracardial (1)    [${rn_lpc_check}] Left paracardial (2)       [${rn_lc_check}] Lesser curvature (3)
+        [${rn_gc_check}] Greater curvature (4)    [${rn_sp_check}] Suprapyloric (5)           [${rn_ip_check}] Infrapyloric (6)
+        [${rn_lga_check}] Left gastric artery (7)  [${rn_cha_check}] Common hepatic artery (8)  [${rn_ca_check}] Celiac artery (9)
+        [${rn_sh_check}] Splenic hilum (10)       [${rn_sa_check}] Splenic artery (11)        [${rn_hp_check}] Hepatic pedicle (12)
+        [${rn_mca_check}] Middle colic artery(15)  [${rn_others_check}] Others: ${txt_rn_others}
 
+`;
+
+    // calculate N stage
     if (has_rln) {
         if (rln_num >= 16) {
             n_stage.push("3b");
@@ -123,16 +138,16 @@ function generate_report(){
     // Distant metastasis
     let has_dm = $('.cb_dm:checked').length > 0;
     report += "5. Distant metastasis (In this study)\n";
-    report += "  [" + (has_dm ? " " : "+") + "] No or Equivocal\n";
-    report += "  [" + (has_dm ? "+" : " ") + "] Yes:\n";
+    report += "    [" + (has_dm ? " " : "+") + "] No or Equivocal\n";
+    report += "    [" + (has_dm ? "+" : " ") + "] Yes:\n";
     let has_nrn = $('.cb_dm_nrn:checked').length > 0;
-    report += "    [" + (has_nrn ? "+" : " ") + "] Non-regional lymph nodes:\n      ";
+    report += "        [" + (has_nrn ? "+" : " ") + "] Non-regional lymph nodes:\n            ";
     $('.cb_dm_nrn').each(function(){
         report += "[" + ($(this).is(':checked') ? "+" : " ") + "] " + $(this).val() + "  ";
     });
     report += "\n";
     let has_non_nrn_dm = $('.cb_dm:not(.cb_dm_nrn):checked').length > 0;
-    report += "    [" + (has_non_nrn_dm ? "+" : " ") + "] Distant organ: ";
+    report += "        [" + (has_non_nrn_dm ? "+" : " ") + "] Distant organ: ";
     report += (has_non_nrn_dm ? join_checkbox_values($('.cb_dm:not(.cb_dm_nrn):checked')) : "___");
     if ($('#cb_dm_others').is(':checked')) {
         if ($('.cb_dm:not("#cb_dm_others, .cb_dm_nrn"):checked').length) {
